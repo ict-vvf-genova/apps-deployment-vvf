326eb57654257cf950f86c980ab5aeb8bef6f435
Emanuele Gissi <egissi@apps.ge.dipvvf.it>
2024-02-02 16:26:02 +0100

    Add variables for ports, and other. Add nodered config.js

--- config/nodered/settings.js
+++ config/nodered/settings.js
@@ -1,74 +1,15 @@
 /**
  * This is the default settings file provided by Node-RED.
  *
- * It can contain any valid JavaScript code that will get run when Node-RED
- * is started.
- *
- * Lines that start with // are commented out.
- * Each entry should be separated from the entries above and below by a comma ','
- *
- * For more information about individual settings, refer to the documentation:
- *    https://nodered.org/docs/user-guide/runtime/configuration
- *
- * The settings are split into the following sections:
- *  - Flow File and User Directory Settings
- *  - Security
- *  - Server Settings
- *  - Runtime Settings
- *  - Editor Settings
- *  - Node Settings
- *
- **/
+ * https://nodered.org/docs/user-guide/runtime/configuration
+  **/
 
 module.exports = {
 
-/*******************************************************************************
- * Flow File and User Directory Settings
- *  - flowFile
- *  - credentialSecret
- *  - flowFilePretty
- *  - userDir
- *  - nodesDir
- ******************************************************************************/
-
-    /** The file containing the flows. If not set, defaults to flows_<hostname>.json **/
     flowFile: 'flows.json',
-
-    /** By default, credentials are encrypted in storage using a generated key. To
-     * specify your own secret, set the following property.
-     * If you want to disable encryption of credentials, set this property to false.
-     * Note: once you set this property, do not change it - doing so will prevent
-     * node-red from being able to decrypt your existing credentials and they will be
-     * lost.
-     */
-    //credentialSecret: "a-secret-key",
-
-    /** By default, the flow JSON will be formatted over multiple lines making
-     * it easier to compare changes when using version control.
-     * To disable pretty-printing of the JSON set the following property to false.
-     */
+    credentialSecret: "${NODERED_ENCRYPTION}",
     flowFilePretty: true,
-
-    /** By default, all user data is stored in a directory called `.node-red` under
-     * the user's home directory. To use a different location, the following
-     * property can be used
-     */
-    //userDir: '/home/nol/.node-red/',
-
-    /** Node-RED scans the `nodes` directory in the userDir to find local node files.
-     * The following property can be used to specify an additional directory to scan.
-     */
-    //nodesDir: '/home/nol/.node-red/nodes',
-
-/*******************************************************************************
- * Security
- *  - adminAuth
- *  - https
- *  - httpsRefreshInterval
- *  - requireHttps
- *  - httpNodeAuth
- *  - httpStaticAuth
- ******************************************************************************/
+    userDir: '/data/',
 
     /** To password protect the Node-RED editor and admin API, the following
      * property can be used. See https://nodered.org/docs/security.html for details.
@@ -82,40 +23,6 @@ module.exports = {
         }]
     },
 
-    /** The following property can be used to enable HTTPS
-     * This property can be either an object, containing both a (private) key
-     * and a (public) certificate, or a function that returns such an object.
-     * See http://nodejs.org/api/https.html#https_https_createserver_options_requestlistener
-     * for details of its contents.
-     */
-
-    /** Option 1: static object */
-    //https: {
-    //  key: require("fs").readFileSync('privkey.pem'),
-    //  cert: require("fs").readFileSync('cert.pem')
-    //},
-
-    /** Option 2: function that returns the HTTP configuration object */
-    // https: function() {
-    //     // This function should return the options object, or a Promise
-    //     // that resolves to the options object
-    //     return {
-    //         key: require("fs").readFileSync('privkey.pem'),
-    //         cert: require("fs").readFileSync('cert.pem')
-    //     }
-    // },
-
-    /** If the `https` setting is a function, the following setting can be used
-     * to set how often, in hours, the function will be called. That can be used
-     * to refresh any certificates.
-     */
-    //httpsRefreshInterval : 12,
-
-    /** The following property can be used to cause insecure HTTP connections to
-     * be redirected to HTTPS.
-     */
-    //requireHttps: true,
-
     /** To password protect the node-defined HTTP endpoints (httpNodeRoot),
      * including node-red-dashboard, or the static content (httpStatic), the
      * following properties can be used.
@@ -125,76 +32,9 @@ module.exports = {
     //httpNodeAuth: {user:"user",pass:"$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN."},
     //httpStaticAuth: {user:"user",pass:"$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN."},
 
-/*******************************************************************************
- * Server Settings
- *  - uiPort
- *  - uiHost
- *  - apiMaxLength
- *  - httpServerOptions
- *  - httpAdminRoot
- *  - httpAdminMiddleware
- *  - httpNodeRoot
- *  - httpNodeCors
- *  - httpNodeMiddleware
- *  - httpStatic
- *  - httpStaticRoot
- ******************************************************************************/
-
-    /** the tcp port that the Node-RED web server is listening on */
     uiPort: process.env.PORT || 1880,
-
-    /** By default, the Node-RED UI accepts connections on all IPv4 interfaces.
-     * To listen on all IPv6 addresses, set uiHost to "::",
-     * The following property can be used to listen on a specific interface. For
-     * example, the following would only allow connections from the local machine.
-     */
-    //uiHost: "127.0.0.1",
-
-    /** The maximum size of HTTP request that will be accepted by the runtime api.
-     * Default: 5mb
-     */
-    //apiMaxLength: '5mb',
-
-    /** The following property can be used to pass custom options to the Express.js
-     * server used by Node-RED. For a full list of available options, refer
-     * to http://expressjs.com/en/api.html#app.settings.table
-     */
-    //httpServerOptions: { },
-
-    /** By default, the Node-RED UI is available at http://localhost:1880/
-     * The following property can be used to specify a different root path.
-     * If set to false, this is disabled.
-     */
-    //httpAdminRoot: '/admin',
-
-    /** The following property can be used to add a custom middleware function
-     * in front of all admin http routes. For example, to set custom http
-     * headers. It can be a single function or an array of middleware functions.
-     */
-    // httpAdminMiddleware: function(req,res,next) {
-    //    // Set the X-Frame-Options header to limit where the editor
-    //    // can be embedded
-    //    //res.set('X-Frame-Options', 'sameorigin');
-    //    next();
-    // },
-
-
-    /** Some nodes, such as HTTP In, can be used to listen for incoming http requests.
-     * By default, these are served relative to '/'. The following property
-     * can be used to specify a different root path. If set to false, this is
-     * disabled.
-     */
-    //httpNodeRoot: '/red-nodes',
-
-    /** The following property can be used to configure cross-origin resource sharing
-     * in the HTTP nodes.
-     * See https://github.com/troygoode/node-cors#configuration-options for
-     * details on its contents. The following is a basic permissive set of options:
-     */
-    //httpNodeCors: {
-    //    origin: "*",
-    //    methods: "GET,PUT,POST,DELETE"
-    //},
+    uiHost: "127.0.0.1",
+    apiMaxLength: '5mb',
 
     /** If you need to set an http proxy please set an environment variable
      * called http_proxy (or HTTP_PROXY) outside of Node-RED in the operating system.
@@ -203,63 +43,6 @@ module.exports = {
      * You may also specify no_proxy (or NO_PROXY) to supply a comma separated
      * list of domains to not proxy, eg - no_proxy=.acme.co,.acme.co.uk
      */
-
-    /** The following property can be used to add a custom middleware function
-     * in front of all http in nodes. This allows custom authentication to be
-     * applied to all http in nodes, or any other sort of common request processing.
-     * It can be a single function or an array of middleware functions.
-     */
-    //httpNodeMiddleware: function(req,res,next) {
-    //    // Handle/reject the request, or pass it on to the http in node by calling next();
-    //    // Optionally skip our rawBodyParser by setting this to true;
-    //    //req.skipRawBodyParser = true;
-    //    next();
-    //},
-
-    /** When httpAdminRoot is used to move the UI to a different root path, the
-     * following property can be used to identify a directory of static content
-     * that should be served at http://localhost:1880/.
-     * When httpStaticRoot is set differently to httpAdminRoot, there is no need
-     * to move httpAdminRoot
-     */
-    //httpStatic: '/home/nol/node-red-static/', //single static source
-    /**
-     *  OR multiple static sources can be created using an array of objects...
-     *  Each object can also contain an options object for further configuration.
-     *  See https://expressjs.com/en/api.html#express.static for available options.
-     */
-    //httpStatic: [
-    //    {path: '/home/nol/pics/',    root: "/img/"},
-    //    {path: '/home/nol/reports/', root: "/doc/"},
-    //    {path: '/home/nol/videos/',  root: "/vid/", options: {maxAge: '1d'}}
-    //],
-
-    /**
-     * All static routes will be appended to httpStaticRoot
-     * e.g. if httpStatic = "/home/nol/docs" and  httpStaticRoot = "/static/"
-     *      then "/home/nol/docs" will be served at "/static/"
-     * e.g. if httpStatic = [{path: '/home/nol/pics/', root: "/img/"}]
-     *      and httpStaticRoot = "/static/"
-     *      then "/home/nol/pics/" will be served at "/static/img/"
-     */
-    //httpStaticRoot: '/static/',
-
-/*******************************************************************************
- * Runtime Settings
- *  - lang
- *  - runtimeState
- *  - diagnostics
- *  - logging
- *  - contextStorage
- *  - exportGlobalContextKeys
- *  - externalModules
- ******************************************************************************/
-
-    /** Uncomment the following to run node-red in your preferred language.
-     * Available languages include: en-US (default), ja, de, zh-CN, zh-TW, ru, ko
-     * Some languages are more complete than others.
-     */
-    // lang: "de",
 
     /** Configure diagnostics options
      * - enabled:  When `enabled` is `true` (or unset), diagnostics data will
@@ -273,6 +56,7 @@ module.exports = {
         /** enable or disable diagnostics display in the node-red editor. Must be set to `false` to disable */
         ui: true,
     },
+
     /** Configure runtimeState options
      * - enabled:  When `enabled` is `true` flows runtime can be Started/Stopped
      *   by POSTing to available at http://localhost:1880/flows/state
@@ -306,17 +90,6 @@ module.exports = {
             audit: false
         }
     },
-
-    /** Context Storage
-     * The following property can be used to enable context storage. The configuration
-     * provided here will enable file-based context that flushes to disk every 30 seconds.
-     * Refer to the documentation for further options: https://nodered.org/docs/api/context/
-     */
-    //contextStorage: {
-    //    default: {
-    //        module:"localfilesystem"
-    //    },
-    //},
 
     /** `global.keys()` returns a list of all properties set in global context.
      * This allows them to be displayed in the Context Sidebar within the editor.
@@ -354,19 +127,6 @@ module.exports = {
         //     denyList: []
         // }
     },
-
-
-/*******************************************************************************
- * Editor Settings
- *  - disableEditor
- *  - editorTheme
- ******************************************************************************/
-
-    /** The following property can be used to disable the editor. The admin API
-     * is not affected by this option. To disable both the editor and the admin
-     * API, use either the httpRoot or httpAdminRoot properties
-     */
-    //disableEditor: false,
 
     /** Customising the editor
      * See https://nodered.org/docs/user-guide/runtime/configuration#editor-themes
